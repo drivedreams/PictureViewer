446756a6d8a188eed6666cba164c53811bc057c4
drivedreams <drivedreams@163.com>
2013-09-15 19:55:47 +0800

    Add category layout

--- picture_viewer/src/com/haihai/picture/viewer/CategoryActivity.java
+++ picture_viewer/src/com/haihai/picture/viewer/CategoryActivity.java
@@ -1,13 +1,20 @@
 package com.haihai.picture.viewer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
 public class CategoryActivity extends Activity {
-	
+	private Map<String, Bitmap> categoryMap = new HashMap<String, Bitmap>();  
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 	 
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.category_layout); 
+		String category_names[] = this.getResources().getStringArray(R.array.category_names);
+		
 	}
 }
